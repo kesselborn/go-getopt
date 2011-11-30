5270b73190fe252db67a44a443217f965792ad75
Daniel Bornkessel <daniel@soundcloud.com>
2011-11-30 21:10:25 +0100

    implement FindOption method

--- getopt.go
+++ getopt.go
@@ -13,53 +13,7 @@ const InvalidOption = 1
 
 const OPTIONS_SEPARATOR = "--"
 
-type Option struct {
-  option_definition string
-  description string
-  flags int
-  default_value string
-}
-
-func (option Option) Key() (key string) {
-  return strings.Split(option.option_definition, "|")[0]
-}
-
-
-func (option Option) LongOpt() (longOpt string) {
-  if option.flags & NoLongOpt == 0 {
-    longOpt = option.Key()
-  }
-
-  return longOpt
-}
-
-func (option Option) ShortOpt() (shortOpt string) {
-  token := strings.Split(option.option_definition, "|")
-
-  if len(token) > 1 {
-    shortOpt = token[1]
-  }
-
-  return shortOpt
-}
-
-func (option Option) EnvVar() (envVar string) {
-  token := strings.Split(option.option_definition, "|")
-
-  if len(token) > 2 {
-    envVar = token[2]
-  }
-
-  return envVar
-}
-
-
-type Options []Option;
-type GetOptError struct {
-  errorCode int
-  message string
-}
-
+// helper functions
 func parseShortOpt(option string) (opt string, val string, found bool) {
   if len(option) > 1 && option[0] == '-' && option[1] >= 'A' && option[1] <= 'z' {
     found = true
@@ -101,6 +55,80 @@ func argumentsEnd(option string) bool {
   return option == "--"
 }
 
-func (optionsDefinition Options) parse(args []string) (map[string] string, []string, []string, *GetOptError) {
-  return make(map[string] string), []string{}, []string{}, nil
+// Option
+type Option struct {
+  option_definition string
+  description string
+  flags int
+  default_value string
 }
+
+func (option Option) eq(other Option) bool {
+  return option.option_definition == other.option_definition &&
+         option.description == other.description &&
+         option.flags == other.flags &&
+         option.default_value == other.default_value
+}
+
+func (option Option) neq(other Option) bool {
+  return !option.eq(other)
+}
+
+func (option Option) Key() (key string) {
+  return strings.Split(option.option_definition, "|")[0]
+}
+
+
+func (option Option) LongOpt() (longOpt string) {
+  if option.flags & NoLongOpt == 0 {
+    longOpt = option.Key()
+  }
+
+  return longOpt
+}
+
+func (option Option) ShortOpt() (shortOpt string) {
+  token := strings.Split(option.option_definition, "|")
+
+  if len(token) > 1 {
+    shortOpt = token[1]
+  }
+
+  return shortOpt
+}
+
+func (option Option) EnvVar() (envVar string) {
+  token := strings.Split(option.option_definition, "|")
+
+  if len(token) > 2 {
+    envVar = token[2]
+  }
+
+  return envVar
+}
+
+type Options []Option;
+
+
+func (options Options) FindOption(optionString string) (option Option, found bool) {
+  for _, cur := range options {
+    if cur.ShortOpt() == optionString || cur.LongOpt() == optionString {
+      option = cur
+      found = true
+      break
+    }
+  }
+
+  return option, found
+}
+
+
+type GetOptError struct {
+  errorCode int
+  message string
+}
+//func (optionsDefinition Options) parse(args []string) (map[string] string, []string, []string, *GetOptError) {
+//  return make(map[string] string), []string{}, []string{}, nil
+//}
+
+//vim:fdm=manual
---
+++ getopt_options_methods_test.go
@@ -0,0 +1,22 @@
+package getopt
+
+import(
+  "testing"
+)
+
+func TestOptionsMethods(t *testing.T) {
+  option1 := Option{"method|m|MON_METHOD", "method: one of either 'heartbeat' or 'nagios'", Required, ""}
+  option2 := Option{"logfile|l"          , "log to file <logfile>"                        , Optional | NoLongOpt, ""}
+  option3 := Option{"verbose"            , "show verbose output"                          , Flag, ""}
+
+  options := Options{ option1, option2, option3 }
+
+  if val, _   := options.FindOption("method"); val.neq(option1) { t.Errorf("couldn't find option 'method'") }
+  if _, found := options.FindOption("method"); found != true    { t.Errorf("couldn't find option 'method'") }
+
+  if val, _   := options.FindOption("m"); val.neq(option1)      { t.Errorf("couldn't find option 'm'") }
+  if _, found := options.FindOption("m"); found != true         { t.Errorf("couldn't find option 'm'") }
+
+  if _, found := options.FindOption("logfile"); found == true   { t.Errorf("shouldn't find an option 'logfile'") }
+}
+
