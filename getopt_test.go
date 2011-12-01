1130e57427a992e8731472a20e2234ecced93155
Daniel Bornkessel <daniel@soundcloud.com>
2011-12-01 01:54:21 +0100

    concatenated options tests

--- getopt_test.go
+++ getopt_test.go
@@ -70,15 +70,67 @@ func TestConcatenatedOptionsParsing(t *testing.T) {
     {"debug|d", "debug mode", Flag, ""},
     {"verbose|v", "verbose mode", Flag, ""},
     {"dryrun|D", "dry run only", Flag, ""},
+    {"logfile|l", "log file", Optional, ""},
+    {"mode|m", "operating mode", Required, ""},
   }
 
   if opts, _, _, _ := options.parse([]string{"-dv"}, "", 0 );
     opts["debug"] != "true" || opts["verbose"] != "true" {
-      t.Errorf("did not recognize all flags when concatenation options")
+      t.Errorf("did not recognize all flags when concatenation options (2 options)")
   }
 
   if opts, _, _, _ := options.parse([]string{"-dvD"}, "", 0 );
     opts["debug"] != "true" || opts["verbose"] != "true" || opts["dryrun"] != "true" {
-      t.Errorf("did not recognize all flags when concatenation options")
+      t.Errorf("did not recognize all flags when concatenation options (3 options)")
   }
+
+  if _, _, _, err := options.parse([]string{"-dvD"}, "", 0 );
+    err == nil || err.errorCode != MissingOption {
+      t.Errorf("did not recognize a missing required option in concatenation mode")
+  }
+
+  if opts, _, _, _ := options.parse([]string{"-dvDl/tmp/log.txt"}, "", 0 );
+    opts["debug"]   != "true" || opts["verbose"] != "true" ||
+    opts["dryrun"]  != "true" || opts["logfile"] != "/tmp/log.txt" {
+      t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{"-dvDl", "/tmp/log.txt"}, "", 0 );
+    opts["debug"]   != "true" || opts["verbose"] != "true" ||
+    opts["dryrun"]  != "true" || opts["logfile"] != "/tmp/log.txt" {
+      t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional separated by space)")
+  }
+
+  if _, _, _, err := options.parse([]string{"-Dl"}, "", 0 );
+    err == nil || err.errorCode != MissingValue {
+      t.Errorf("did not realize that I missed a value")
+  }
+
+  if _, _, _, err := options.parse([]string{"-Dl", "-d"}, "", 0 );
+    err == nil || err.errorCode != MissingValue {
+      t.Errorf("did not realize that I missed a value")
+  }
+
+  if opts, _, _, _ := options.parse([]string{"-dvDl", "/tmp/log.txt"}, "", 0 );
+    opts["debug"]   != "true" || opts["verbose"] != "true" ||
+    opts["dryrun"]  != "true" || opts["logfile"] != "/tmp/log.txt" {
+      t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional separated by space)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{"-dvDmdaemon"}, "", 0 );
+    opts["debug"]   != "true" || opts["verbose"] != "true" ||
+    opts["dryrun"]  != "true" || opts["mode"]    != "daemon" {
+      t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{"-dvDm", "daemon"}, "", 0 );
+    opts["debug"]   != "true" || opts["verbose"] != "true" ||
+    opts["dryrun"]  != "true" || opts["mode"]    != "daemon" {
+      t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required separated by space)")
+  }
+
+}
+
+func TestEnvVars(t *testing.T) {
+  t.Errorf("env var tests missing")
 }
