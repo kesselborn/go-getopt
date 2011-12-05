5aa161ab57db4f976a8c9f52647f7af69c171294
Daniel Bornkessel <daniel@soundcloud.com>
2011-12-05 09:44:52 +0100

    implement overwrites array

--- getopt.go
+++ getopt.go
@@ -1,9 +1,14 @@
 package getopt
+import (
+  "strings"
+//  "fmt"
+)
 
 const Required = 1
 const Optional = 2
 const Flag = 4
 const NoLongOpt = 8
+const ExampleIsDefault = 16
 
 const InvalidOption = 1
 const MissingValue = 2
@@ -19,85 +24,128 @@ type GetOptError struct {
   message string
 }
 
+func mapifyEnviron(environment []string) (envArray map[string] string) {
+  envArray = make(map[string] string)
+
+  for _, cur := range environment {
+    envVar := strings.Split(cur, "=")
+    if len(envVar) > 1 {
+      envArray[envVar[0]] = envVar[1]
+    }
+  }
+
+  return
+}
+
+func (optionsDefinition Options) setOverwrites(options map[string] OptionValue, overwrites []string) (err *GetOptError) {
+  overwritesMap := mapifyEnviron(overwrites)
+  acceptedEnvVars := make(map[string] Option)
+
+  for _, opt := range optionsDefinition {
+    if value := opt.EnvVar(); value != "" {
+      acceptedEnvVars[value] = opt
+    }
+  }
+
+  for key, acceptedEnvVar := range acceptedEnvVars {
+    if value := overwritesMap[key]; value != "" {
+      options[acceptedEnvVar.LongOpt()], err = assignValue(acceptedEnvVar.default_value, value)
+      if err != nil {
+        break
+      }
+    }
+  }
+
+  return
+}
+
 
 func (optionsDefinition Options) parse(args []string,
+                                       defaults []string,
                                        description string,
                                        flags int) (
                                   options map[string] OptionValue,
                                   arguments []string,
                                   passThrough []string,
                                   err *GetOptError) {
+
   options = make(map[string] OptionValue)
 
+  // all flags are false by default
   for _, flagOption := range optionsDefinition.FlagOptions() {
     options[flagOption], err = assignValue(false, "false")
   }
 
-  for i:=0; i < len(args); i++ {
-    var opt, val string
-    var found bool
+  // set overwrites
+  err = optionsDefinition.setOverwrites(options, defaults)
 
-    token := args[i]
+  if err == nil {
+    for i:=0; i < len(args); i++ {
+      var opt, val string
+      var found bool
 
-    if argumentsEnd(token) {
-      break
-    }
+      token := args[i]
 
-    if isValue(token) {
-      arguments = args[i:]
-      break
-    }
-
-    opt, val, found = parseShortOpt(token)
-
-    if found {
-      buffer := token
-      for found && optionsDefinition.IsFlag(opt) && len(buffer) > 2 {
-        // concatenated options ... continue parsing
-        currentOption, _ := optionsDefinition.FindOption(opt)
-        key := currentOption.Key()
-
-        options[key], err = assignValue(currentOption.default_value, "true")
-
-        // make it look as if we have a normal option with a '-' prefix
-        buffer = "-" + buffer[2:]
-        opt, val, found = parseShortOpt(buffer)
-      }
-
-    } else {
-      opt, val, found = parseLongOpt(token)
-    }
-
-    currentOption, found := optionsDefinition.FindOption(opt)
-    key := currentOption.Key()
-
-    if !found {
-      err = &GetOptError{ InvalidOption, "invalid option '" + token + "'" }
-      break
-    }
-
-    if optionsDefinition.IsFlag(opt) {
-      options[key], err = assignValue(true, "true")
-    } else {
-      if val == "" {
-        if len(args) > i + 1 && isValue(args[i + 1]) {
-          i = i + 1
-          val = args[i]
-        } else {
-          err = &GetOptError{ MissingValue, "Option '" + token + "' needs a value" }
-          break
-        }
-      }
-
-
-      if !isValue(val) {
-        err = &GetOptError{ InvalidValue, "Option '" + token + "' got invalid value: '" + val + "'" }
+      if argumentsEnd(token) {
         break
       }
 
-      options[key], err = assignValue(currentOption.default_value, val)
-    }
+      if isValue(token) {
+        arguments = args[i:]
+        break
+      }
 
+      opt, val, found = parseShortOpt(token)
+
+      if found {
+        buffer := token
+        for found && optionsDefinition.IsFlag(opt) && len(buffer) > 2 {
+          // concatenated options ... continue parsing
+          currentOption, _ := optionsDefinition.FindOption(opt)
+          key := currentOption.Key()
+
+          options[key], err = assignValue(currentOption.default_value, "true")
+
+          // make it look as if we have a normal option with a '-' prefix
+          buffer = "-" + buffer[2:]
+          opt, val, found = parseShortOpt(buffer)
+        }
+
+      } else {
+        opt, val, found = parseLongOpt(token)
+      }
+
+      currentOption, found := optionsDefinition.FindOption(opt)
+      key := currentOption.Key()
+
+      if !found {
+        err = &GetOptError{ InvalidOption, "invalid option '" + token + "'" }
+        break
+      }
+
+      if optionsDefinition.IsFlag(opt) {
+        options[key], err = assignValue(true, "true")
+      } else {
+        if val == "" {
+          if len(args) > i + 1 && isValue(args[i + 1]) {
+            i = i + 1
+            val = args[i]
+          } else {
+            err = &GetOptError{ MissingValue, "Option '" + token + "' needs a value" }
+            break
+          }
+        }
+
+
+        if !isValue(val) {
+          err = &GetOptError{ InvalidValue, "Option '" + token + "' got invalid value: '" + val + "'" }
+          break
+        }
+
+        options[key], err = assignValue(currentOption.default_value, val)
+      }
+
+    }
   }
 
   if err == nil {
@@ -107,7 +155,6 @@ func (optionsDefinition Options) parse(args []string,
         break
       }
     }
-
   }
 
   return
--- getopt_test.go
+++ getopt_test.go
@@ -2,13 +2,6 @@ package getopt
 import "testing"
 import "fmt"
 
-func printMap(_map map[string] string) {
-  fmt.Println("map:")
-  for key, val := range _map {
-    fmt.Println(" '" + key + "': '" + val + "'")
-  }
-}
-
 func TestShortOptionsFlagsParsing(t *testing.T) {
   options := Options{
     {"debug|d", "debug mode", Flag, ""},
@@ -16,17 +9,17 @@ func TestShortOptionsFlagsParsing(t *testing.T) {
     {"dryrun|D", "dry run only", Flag, ""},
   }
 
-  if opts, _, _, _ := options.parse([]string{"-d"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-d"}, []string{}, "", 0 );
     opts["debug"].Bool != true {
       t.Errorf("debug flag was not set")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-d"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-d"},[]string{},  "", 0 );
     opts["debug"].Bool != true || opts["verbose"].Bool != false {
       t.Errorf("verbose flag was not set to false by default")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-d", "-v"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-d", "-v"}, []string{}, "", 0 );
     opts["debug"].Bool != true || opts["verbose"].Bool != true {
       t.Errorf("did not recognize all flags")
   }
@@ -38,27 +31,27 @@ func TestShortOptionRequiredParsing(t *testing.T) {
     {"method|m|MON_METHOD", "method: one of either 'heartbeat' or 'nagios'", Required, ""},
   }
 
-  if opts, _, _, _ := options.parse([]string{"-m", "heartbeat"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-m", "heartbeat"}, []string{}, "", 0 );
     opts["method"].String != "heartbeat" {
       t.Errorf("method optioned wasn't parsed correctly expected 'heartbeat', was '" + opts["method"].String + "'")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-mheartbeat"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-mheartbeat"}, []string{}, "", 0 );
     opts["method"].String != "heartbeat" {
       t.Errorf("method optioned wasn't parsed correctly expected 'heartbeat', was '" + opts["method"].String + "'")
   }
 
-  if _, _, _, err := options.parse([]string{"-m"}, "", 0 );
+  if _, _, _, err := options.parse([]string{"-m"}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingValue {
       t.Errorf("required option without value did not raise error")
   }
 
-  if _, _, _, err := options.parse([]string{"-m", "-x"}, "", 0 );
+  if _, _, _, err := options.parse([]string{"-m", "-x"}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingValue {
       t.Errorf("required option without value did not raise error")
   }
 
-  if _, _, _, err := options.parse([]string{""}, "", 0 );
+  if _, _, _, err := options.parse([]string{""}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingOption {
       t.Errorf("required option wasn't set")
   }
@@ -74,27 +67,27 @@ func TestConcatenatedOptionsParsingSimple(t *testing.T) {
     {"mode|m", "operating mode", Required, ""},
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dv"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dv"}, []string{}, "", 0 );
     opts["debug"].Bool != true || opts["verbose"].Bool != true {
       t.Errorf("did not recognize all flags when concatenation options (2 options)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvD"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvD"}, []string{}, "", 0 );
     opts["debug"].Bool != true || opts["verbose"].Bool != true || opts["dryrun"].Bool != true {
       t.Errorf("did not recognize all flags when concatenation options (3 options)")
   }
 
-  if _, _, _, err := options.parse([]string{"-dvD"}, "", 0 );
+  if _, _, _, err := options.parse([]string{"-dvD"}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingOption {
       t.Errorf("did not recognize a missing required option in concatenation mode")
   }
 
-  if _, _, _, err := options.parse([]string{"-Dl"}, "", 0 );
+  if _, _, _, err := options.parse([]string{"-Dl"}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingValue {
       t.Errorf("did not realize that I missed a value")
   }
 
-  if _, _, _, err := options.parse([]string{"-Dl", "-d"}, "", 0 );
+  if _, _, _, err := options.parse([]string{"-Dl", "-d"}, []string{}, "", 0 );
     err == nil || err.errorCode != MissingValue {
       t.Errorf("did not realize that I missed a value")
   }
@@ -109,25 +102,25 @@ func TestConcatenatedOptionsParsingWithStringValueOptionAtTheEnd(t *testing.T) {
     {"logfile|l", "log file", Optional, ""},
     {"mode|m", "operating mode", Required, ""},
   }
-  if opts, _, _, _ := options.parse([]string{"-dvDl/tmp/log.txt"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDl/tmp/log.txt"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["logfile"].String != "/tmp/log.txt" {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDl", "/tmp/log.txt"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDl", "/tmp/log.txt"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["logfile"].String != "/tmp/log.txt" {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional separated by space)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDmdaemon"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDmdaemon"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["mode"].String    != "daemon" {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDm", "daemon"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDm", "daemon"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["mode"].String    != "daemon" {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required separated by space)")
@@ -143,32 +136,32 @@ func TestConcatenatedOptionsParsingWithIntValueOptionAtTheEnd(t *testing.T) {
     {"port|p", "port", Optional, 3000},
     {"instances|i", "instances", Required, 1},
   }
-  if opts, _, _, _ := options.parse([]string{"-dvDp3000"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDp3000"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["port"].Int != 3000 {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional int)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDp", "3000"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDp", "3000"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["port"].Int != 3000 {
       fmt.Printf("%#v", opts)
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional int separated by space)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDp", "3000"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDp", "3000"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["port"].Int != 3000 {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional int separated by space)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDi4"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDi4"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["instances"].Int    != 4 {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDi", "4"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDi", "4"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || opts["instances"].Int    != 4 {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required int separated by space)")
@@ -184,30 +177,82 @@ func TestConcatenatedOptionsParsingWithIntArrayValueOptionAtTheEnd(t *testing.T)
     {"ports|p", "ports", Optional, []int{3000, 3001, 3002}},
     {"timeouts|t", "timeouts", Required, []int{1,2,4,10,30}},
   }
-  if opts, _, _, _ := options.parse([]string{"-dvDp5000,5001,5002"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDp5000,5001,5002"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || !equalIntArray(opts["ports"].IntArray, []int64{5000, 5001, 5002}) {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional int array)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDp", "5000,5001,5002"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDp", "5000,5001,5002"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || !equalIntArray(opts["ports"].IntArray, []int64{5000, 5001, 5002}) {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 optional int array separated by space)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDt10,20,30"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDt10,20,30"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || !equalIntArray(opts["timeouts"].IntArray, []int64{10,20,30}) {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required int array)")
   }
 
-  if opts, _, _, _ := options.parse([]string{"-dvDt", "10,20,30"}, "", 0 );
+  if opts, _, _, _ := options.parse([]string{"-dvDt", "10,20,30"}, []string{}, "", 0 );
     opts["debug"].Bool   != true || opts["verbose"].Bool != true ||
     opts["dryrun"].Bool  != true || !equalIntArray(opts["timeouts"].IntArray, []int64{10,20,30}) {
       t.Errorf("did not recognize all flags when concatenation options (3 flags + 1 required int array separated by space)")
   }
 
+}
+
+func TestEnvironmentValueParsing(t *testing.T) {
+  options := Options{
+    {"debug|d|DEBUG", "debug mode", Flag, true},
+    {"ports|p|PORTS", "ports", Required, []int{3000, 3001, 3002}},
+    {"instances||INSTANCES", "instances", Optional, 4},
+    {"keys||KEYS", "keys", Optional, []string{"foo,bar,baz"}},
+    {"logfile||LOGFILE", "ports", Optional | ExampleIsDefault, "/var/log/foo.log"},
+  }
+
+  if opts, _, _, _ := options.parse([]string{}, []string{"DEBUG=1"}, "", 0);
+    opts["debug"].Bool != true {
+      t.Errorf("did not recognize option set via ENV variable (DEBUG=1)")
+  }
+  if opts, _, _, _ := options.parse([]string{}, []string{"DEBUG=TRUE"}, "", 0);
+    opts["debug"].Bool != true {
+      t.Errorf("did not recognize option set via ENV variable (DEBUG=TRUE)")
+  }
+  if opts, _, _, _ := options.parse([]string{}, []string{"DEBUG=true"}, "", 0);
+    opts["debug"].Bool != true {
+      t.Errorf("did not recognize option set via ENV variable (DEBUG=true)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{}, []string{"PORTS=4000,4001,4002"}, "", 0);
+    !equalIntArray(opts["ports"].IntArray, []int64{4000,4001,4002}) {
+      t.Errorf("did not recognize option set via ENV variable (PORTS=4000,4001,4002)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{}, []string{"KEYS=faa,bor,boz"}, "", 0);
+    !equalStringArray(opts["keys"].StrArray, []string{"faa","bor","boz"}) {
+      t.Errorf("did not recognize option set via ENV variable (KEYS=faa,bor,boz)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{}, []string{"LOGFILE=/tmp/logfile"}, "", 0);
+    opts["logfile"].String != "/tmp/logfile" {
+      t.Errorf("did not recognize option set via ENV variable (LOGFILE=/tmp/lofile)")
+  }
+
+  if opts, _, _, _ := options.parse([]string{}, []string{"INSTANCES=13"}, "", 0);
+    opts["instances"].Int != 13 {
+      fmt.Printf("%#v" , opts)
+      t.Errorf("did not recognize option set via ENV variable (INSTANCES=13)")
+  }
+}
+
+func TestDefaultValueParsing(t *testing.T) {
+//  options := Options{
+//    {"debug|d|DEBUG", "debug mode", Flag, true},
+//    {"ports|p|PORT", "ports", Required, []int{3000, 3001, 3002}},
+//    {"logfile||LOGFILE", "ports", Optional | ExampleIsDefault, "/var/log/foo.log"},
+//  }
 }
 
 func TestEnvVars(t *testing.T) {
