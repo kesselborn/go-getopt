cfa665f85f404027314a81d1e88077f1b23fc4d9
Daniel Bornkessel <daniel@soundcloud.com>
2011-12-12 21:44:54 +0100

    add copyright info and license

--- option_stringifier.go
+++ option_stringifier.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2011, SoundCloud Ltd., Daniel Bornkessel
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+// Source code and contact info at http://github.com/kesselborn/go-getopt
+
 package getopt
 
 import(
