baf50d7c6490c3d342ff534f0bc0ba2586d2393c
Daniel Bornkessel <daniel@soundcloud.com>
2011-12-13 01:36:25 +0100

    move from VALUE to <value>

--- option_stringifier_test.go
+++ option_stringifier_test.go
@@ -11,32 +11,32 @@ import (
 
 func TestUsageOutput(t *testing.T) {
 	if got, expected := (Option{"method", "...", IsArg | Optional, ""}.Usage()),
-		"[METHOD]"; got != expected {
+		"[<method>]"; got != expected {
 		t.Errorf("Error creating usage text (argument):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "...", IsArg, ""}.Usage()),
-		"METHOD"; got != expected {
+		"<method>"; got != expected {
 		t.Errorf("Error creating usage text (argument):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "...", Required, ""}.Usage()),
-		"-m METHOD"; got != expected {
+		"-m <method>"; got != expected {
 		t.Errorf("Error creating usage text (Required):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "...", Required, ""}.Usage()),
-		"--method=METHOD"; got != expected {
+		"--method=<method>"; got != expected {
 		t.Errorf("Error creating usage text (Required, no short):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "...", Optional, ""}.Usage()),
-		"[-m METHOD]"; got != expected {
+		"[-m <method>]"; got != expected {
 		t.Errorf("Error creating usage text (Optional):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "...", Optional, ""}.Usage()),
-		"[--method=METHOD]"; got != expected {
+		"[--method=<method>]"; got != expected {
 		t.Errorf("Error creating usage text (Optional, no short):\ngot:      " + got + "\nexpected: " + expected)
 	}
 
@@ -64,73 +64,73 @@ func TestUsageOutput(t *testing.T) {
 
 func TestBasicOutput(t *testing.T) {
 	if got, expected := (Option{"method|m", "method: one of either 'heartbeat' or 'nagios'", Required | NoLongOpt, ""}.HelpText(20)),
-		"    -m METHOD                  method: one of either 'heartbeat' or 'nagios'"; got != expected {
+		"    -m <method>                method: one of either 'heartbeat' or 'nagios'"; got != expected {
 		t.Errorf("Error stringifying option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "method: one of either 'heartbeat' or 'nagios'", Required, ""}.HelpText(20)),
-		"        --method=METHOD        method: one of either 'heartbeat' or 'nagios'"; got != expected {
+		"        --method=<method>      method: one of either 'heartbeat' or 'nagios'"; got != expected {
 		t.Errorf("Error stringifying option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "method: one of either 'heartbeat' or 'nagios'", Required, ""}.HelpText(20)),
-		"    -m, --method=METHOD        method: one of either 'heartbeat' or 'nagios'"; got != expected {
+		"    -m, --method=<method>      method: one of either 'heartbeat' or 'nagios'"; got != expected {
 		t.Errorf("Error stringifying option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
 
 func TestBasicOutputWithExampleHelpTextValue(t *testing.T) {
 	if got, expected := (Option{"method|m", "method", Optional | NoLongOpt, "heartbeat"}.HelpText(20)),
-		"    -m METHOD                  method (e.g. heartbeat)"; got != expected {
+		"    -m <method>                method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "method", Optional, "heartbeat"}.HelpText(20)),
-		"        --method=METHOD        method (e.g. heartbeat)"; got != expected {
+		"        --method=<method>      method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "method", Optional, "heartbeat"}.HelpText(20)),
-		"    -m, --method=METHOD        method (e.g. heartbeat)"; got != expected {
+		"    -m, --method=<method>      method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "method", Required | NoLongOpt, "heartbeat"}.HelpText(20)),
-		"    -m METHOD                  method (e.g. heartbeat)"; got != expected {
+		"    -m <method>                method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying required option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "method", Required, "heartbeat"}.HelpText(20)),
-		"        --method=METHOD        method (e.g. heartbeat)"; got != expected {
+		"        --method=<method>      method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying required option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "method", Required, "heartbeat"}.HelpText(20)),
-		"    -m, --method=METHOD        method (e.g. heartbeat)"; got != expected {
+		"    -m, --method=<method>      method (e.g. heartbeat)"; got != expected {
 		t.Errorf("Error stringifying required option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
 
 func TestBasicOutputWithDefaultHelpText(t *testing.T) {
 	if got, expected := (Option{"method|m", "method", Optional | ExampleIsDefault | NoLongOpt, "heartbeat"}.HelpText(20)),
-		"    -m METHOD                  method (default: heartbeat)"; got != expected {
+		"    -m <method>                method (default: heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method", "method", Optional | ExampleIsDefault, "heartbeat"}.HelpText(20)),
-		"        --method=METHOD        method (default: heartbeat)"; got != expected {
+		"        --method=<method>      method (default: heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"method|m", "method", Optional | ExampleIsDefault, "heartbeat"}.HelpText(20)),
-		"    -m, --method=METHOD        method (default: heartbeat)"; got != expected {
+		"    -m, --method=<method>      method (default: heartbeat)"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
 
 func TestOutputWithEnvVar(t *testing.T) {
 	if got, expected := (Option{"method|m|METHOD", "method", Optional | ExampleIsDefault | NoLongOpt, "heartbeat"}.HelpText(20)),
-		"    -m METHOD                  method (default: heartbeat); setable via $METHOD"; got != expected {
+		"    -m <method>                method (default: heartbeat); setable via $METHOD"; got != expected {
 		t.Errorf("Error stringifying optional option:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
@@ -142,24 +142,24 @@ func TestOutputArgument(t *testing.T) {
 	}
 
 	if got, expected := (Option{"logfile", "dump log into this file", IsArg, ""}.HelpText(20)),
-		"    LOGFILE                    dump log into this file"; got != expected {
+		"    <logfile>                  dump log into this file"; got != expected {
 		t.Errorf("Error stringifying argument:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"logfile", "dump log into this file", IsArg, "/tmp/foo"}.HelpText(20)),
-		"    LOGFILE                    dump log into this file (e.g. /tmp/foo)"; got != expected {
+		"    <logfile>                  dump log into this file (e.g. /tmp/foo)"; got != expected {
 		t.Errorf("Error stringifying argument:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
 
 func TestOutputPassThrough(t *testing.T) {
 	if got, expected := (Option{"pass through args", "pass through arguments", IsPassThrough, ""}.Usage()),
-		"-- PASS THROUGH ARGS"; got != expected {
+		"-- <pass through args>"; got != expected {
 		t.Errorf("Error stringifying argument:\ngot:      " + got + "\nexpected: " + expected)
 	}
 
 	if got, expected := (Option{"pass through args", "pass through arguments", IsPassThrough, ""}.HelpText(20)),
-		"    PASS THROUGH ARGS          pass through arguments"; got != expected {
+		"    <pass through args>        pass through arguments"; got != expected {
 		t.Errorf("Error stringifying argument:\ngot:      " + got + "\nexpected: " + expected)
 	}
 }
