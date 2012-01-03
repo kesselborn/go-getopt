75a268bf6d3d4e9b1d19106418839e0dedfc5847
Daniel Bornkessel <daniel@soundcloud.com>
2012-01-03 15:08:58 +0100

    make sure that we don't have required arguments after optional arguments

--- consistency_check_test.go
+++ consistency_check_test.go
@@ -6,9 +6,13 @@
 
 package getopt
 
-import "testing"
+import (
+	"testing"
+	"os"
+)
 
 func TestConsistencyChecking(t *testing.T) {
+	os.Args = []string{"prog"}
 	if _, _, _, err := (Options{{"verbose", "...", Optional | Required, ""}}.ParseCommandLine()); err == nil || err.ErrorCode != ConsistencyError {
 		t.Errorf("flags Optional & Required did not raise error!")
 	}
@@ -23,5 +27,16 @@ func TestConsistencyChecking(t *testing.T) {
 	}
 	if _, _, _, err := (Options{{"verbose", "...", IsArg | Flag, ""}}.ParseCommandLine()); err == nil || err.ErrorCode != ConsistencyError {
 		t.Errorf("flags IsArg & Flag")
+	}
+	if _, _, _, err := (Options{{"verbose", "...", IsArg, ""}}.ParseCommandLine()); err == nil || err.ErrorCode != ConsistencyError {
+		t.Errorf("flags IsArg without Required or Optional")
+	}
+	if _, _, _, err := (Options{{"arg1", "...", IsArg | Optional, ""}, {"arg1", "...", IsArg | Required, ""}}.ParseCommandLine()); err == nil || err.ErrorCode != ConsistencyError {
+		t.Errorf("required arg following an optional arg did not raise error")
+	}
+
+	os.Args = []string{"prog", "lirum", "larum"}
+	if _, _, _, err := (Options{{"arg1", "...", IsArg | Required, ""}, {"arg1", "...", IsArg | Optional, ""}}.ParseCommandLine()); err != nil {
+		t.Errorf("required arg followed by an optional arg did raise error: %#v", err)
 	}
 }
