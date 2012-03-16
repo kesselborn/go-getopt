dac5ed471b49581a6cc1bcd62d071e71b0269c94
Daniel Bornkessel <daniel@soundcloud.com>
2012-03-16 12:57:19 +0100

    adjust example programs

--- simple-example/example.go
+++ simple-example/example.go
@@ -8,31 +8,33 @@ import (
 
 func main() {
 	optionDefinition := getopt.Options{
-		{"debug|d|DEBUG", "debug mode", getopt.Optional | getopt.Flag, false},
-		{"config|c", "config file", getopt.IsConfigFile | getopt.ExampleIsDefault, "./config_sample.conf"},
-		{"ports|p|PORTS", "ports", getopt.Optional | getopt.ExampleIsDefault, []int64{3000, 3001, 3002}},
-		{"sports|s|SECONDARY_PORTS", "secondary ports", getopt.Optional | getopt.NoLongOpt, []int{5000, 5001, 5002}},
-		{"instances|i|INSTANCES", "instances", getopt.Required, 4},
-		{"keys||KEYS", "keys", getopt.Required, []string{"foo", "bar", "baz"}},
-		{"logfile||LOGFILE", "logfile", getopt.Optional | getopt.NoEnvHelp, "/var/log/foo.log"},
-		{"file", "files", getopt.IsArg, ""},
-		{"directories", "directories", getopt.IsArg | getopt.Optional, ""},
-		{"pass through", "pass through arguments", getopt.IsPassThrough | getopt.Optional, ""},
+    "description",
+    getopt.Definitions{
+      {"debug|d|DEBUG", "debug mode", getopt.Optional | getopt.Flag, false},
+      {"config|c", "config file", getopt.IsConfigFile | getopt.ExampleIsDefault, "./config_sample.conf"},
+      {"ports|p|PORTS", "ports", getopt.Optional | getopt.ExampleIsDefault, []int64{3000, 3001, 3002}},
+      {"sports|s|SECONDARY_PORTS", "secondary ports", getopt.Optional | getopt.NoLongOpt, []int{5000, 5001, 5002}},
+      {"instances|i|INSTANCES", "instances", getopt.Required, 4},
+      {"keys||KEYS", "keys", getopt.Required, []string{"foo", "bar", "baz"}},
+      {"logfile||LOGFILE", "logfile", getopt.Optional | getopt.NoEnvHelp, "/var/log/foo.log"},
+      {"file", "files", getopt.IsArg | getopt.Required, ""},
+      {"directories", "directories", getopt.IsArg | getopt.Optional, ""},
+      {"pass through", "pass through arguments", getopt.IsPassThrough | getopt.Optional, ""},
+    },
 	}
 
 	options, arguments, passThrough, e := optionDefinition.ParseCommandLine()
 
 	if e != nil {
 		exit_code := 0
-		description := "this is a small sample application for getopt demonstration"
 
 		switch {
 		case e.ErrorCode == getopt.WantsUsage:
 			fmt.Print(optionDefinition.Usage())
 		case e.ErrorCode == getopt.WantsHelp:
-			fmt.Print(optionDefinition.Help(description))
+			fmt.Print(optionDefinition.Help())
 		default:
-			fmt.Println("**** Error: ", e.Message, "\n", optionDefinition.Help(description))
+			fmt.Println("**** Error: ", e.Message, "\n", optionDefinition.Help())
 			exit_code = e.ErrorCode
 		}
 		os.Exit(exit_code)
--- subcommand-example/example.go
+++ subcommand-example/example.go
@@ -1,45 +1,58 @@
 package main
 
 import (
-	"fmt"
-	getopt "github.com/kesselborn/go-getopt"
-	"os"
+  "fmt"
+  getopt "github.com/kesselborn/go-getopt"
+  "os"
 )
 
 func main() {
-	sco := getopt.SubCommandOptions{
-		"*": {
-			{"foo|f", "some arg", getopt.Optional, ""},
-			{"command", "command to execute", getopt.IsSubcommand, ""}},
-		"getenv": {
-			{"bar|b", "some arg", getopt.Optional, ""},
-			{"name", "app's name", getopt.IsArg | getopt.Required, ""},
-			{"key", "environment variable's name", getopt.IsArg | getopt.Required, ""}},
-		"register": {
-			{"deploytype|t", "deploy type (one of mount, bazapta, lxc)", getopt.NoLongOpt | getopt.Optional | getopt.ExampleIsDefault, "lxc"},
-			{"name|n", "app's name", getopt.IsArg | getopt.Required, ""}},
-	}
+  sco := getopt.SubCommandOptions{
+    getopt.Options{
+      "global description",
+      getopt.Definitions{
+        {"foo|f", "some arg", getopt.Optional, ""},
+        {"command", "command to execute", getopt.IsSubcommand, ""},
+      },
+    },
+    getopt.SubCommands{
+      "getenv": {
+        "getenv description",
+        getopt.Definitions{
+          {"bar|b", "some arg", getopt.Optional, ""},
+          {"name", "app's name", getopt.IsArg | getopt.Required, ""},
+          {"key", "environment variable's name", getopt.IsArg | getopt.Required, ""},
+        },
+      },
+      "register": {
+        "register description",
+        getopt.Definitions{
+          {"deploytype|t", "deploy type (one of mount, bazapta, lxc)", getopt.NoLongOpt | getopt.Optional | getopt.ExampleIsDefault, "lxc"},
+          {"name|n", "app's name", getopt.IsArg | getopt.Required, ""},
+        },
+      },
+    },
+  }
 
-	scope, options, arguments, passThrough, e := sco.ParseCommandLine()
+  scope, options, arguments, passThrough, e := sco.ParseCommandLine()
 
-	if e != nil {
-		exit_code := 0
-		description := "this is a small sample application for getopt demonstration"
+  if e != nil {
+    exit_code := 0
 
-		switch {
-		case e.ErrorCode == getopt.WantsUsage:
-			fmt.Print(sco.Usage(scope))
-		case e.ErrorCode == getopt.WantsHelp:
-			fmt.Print(sco.Help(description, scope))
-		default:
-			fmt.Println(sco.Help(description, scope), "\n", "**** Error: ", e.Message, "\n")
-			exit_code = e.ErrorCode
-		}
-		os.Exit(exit_code)
-	}
+    switch {
+    case e.ErrorCode == getopt.WantsUsage:
+      fmt.Print(sco.Usage(scope))
+    case e.ErrorCode == getopt.WantsHelp:
+      fmt.Print(sco.Help(scope))
+    default:
+      fmt.Println(sco.Help(scope), "\n", "**** Error: ", e.Message, "\n")
+      exit_code = e.ErrorCode
+    }
+    os.Exit(exit_code)
+  }
 
-	fmt.Printf("scope:\n%s", scope)
-	fmt.Printf("options:\n%#v", options)
-	fmt.Printf("arguments: %#v\n", arguments)
-	fmt.Printf("passThrough: %#v\n", passThrough)
+  fmt.Printf("scope:\n%s", scope)
+  fmt.Printf("options:\n%#v", options)
+  fmt.Printf("arguments: %#v\n", arguments)
+  fmt.Printf("passThrough: %#v\n", passThrough)
 }
