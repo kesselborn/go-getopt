996131eb22b29dbeac149eb06366149b5bffe091
Daniel Bornkessel <daniel@soundcloud.com>
2012-03-27 09:52:54 +0200

    add subsubcommand_options example

---
+++ subsubcommand-example/example.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+  "fmt"
+  getopt "github.com/kesselborn/go-getopt"
+  "os"
+)
+
+func main() {
+  ssco := getopt.SubSubCommandOptions{
+		getopt.Options{
+			"global description",
+			getopt.Definitions{
+				{"config|c", "config file", getopt.IsConfigFile | getopt.ExampleIsDefault, "/etc/visor.conf"},
+				{"server|s", "doozer server", getopt.Optional, ""},
+				{"scope", "scope", getopt.IsSubCommand, ""},
+			},
+		},
+		getopt.Scopes{
+			"app": {
+				getopt.Options{
+					"app description",
+					getopt.Definitions{
+						{"foo|f", "a param", getopt.Optional, ""},
+						{"command", "command to execute", getopt.IsSubCommand, ""},
+					},
+				},
+				getopt.SubCommands{
+					"getenv": {
+						"app getenv description",
+						getopt.Definitions{
+							{"key", "environment variable's name", getopt.IsArg | getopt.Required, ""},
+						},
+					},
+					"setenv": {
+						"app setenv description",
+						getopt.Definitions{
+							{"persist|p", "persist this", getopt.Optional | getopt.Flag | getopt.NoLongOpt, ""},
+							{"alias|a", "alias name", getopt.Optional, ""},
+							{"name", "app name", getopt.IsArg | getopt.Required, ""},
+							{"key", "env key", getopt.IsArg | getopt.Required, ""},
+							{"value", "env value", getopt.IsArg | getopt.Optional, ""},
+						},
+					},
+				},
+			},
+			"revision": {
+				getopt.Options{
+					"app revision description",
+					getopt.Definitions{
+						{"rev|r", "revision", getopt.IsArg | getopt.Required, ""},
+						{"command", "command to execute", getopt.IsSubCommand, ""},
+					},
+				},
+				getopt.SubCommands{
+					"list": {
+						"list revisions",
+						getopt.Definitions{
+							{"all|a", "long list output", getopt.Flag, ""},
+						},
+					},
+				},
+			},
+		},
+	}
+
+  scope, subCommand, options, arguments, passThrough, e := ssco.ParseCommandLine()
+
+  if e != nil {
+    exit_code := 0
+
+    switch {
+    case e.ErrorCode == getopt.WantsUsage:
+      fmt.Print(ssco.Usage())
+    case e.ErrorCode == getopt.WantsHelp:
+      fmt.Print(ssco.Help())
+    default:
+      fmt.Println(ssco.Help(), "\n", "**** Error: ", e.Message, "\n")
+      exit_code = e.ErrorCode
+    }
+    os.Exit(exit_code)
+  }
+
+  fmt.Printf("scope:\n%s", scope)
+  fmt.Printf("subCommand:\n%s", subCommand)
+  fmt.Printf("options:\n%#v", options)
+  fmt.Printf("arguments: %#v\n", arguments)
+  fmt.Printf("passThrough: %#v\n", passThrough)
+}
